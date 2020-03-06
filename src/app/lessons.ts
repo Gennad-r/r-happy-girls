2656c579cc1c58554651c8738f24e6e7690cbe5b
Gennad-r <gennad.r@gmail.com>
2020-03-06 21:49:54 +0200

    url fix

--- src/app/lessons.ts
+++ src/app/lessons.ts
@@ -1,78 +1,78 @@
-import { ILesson } from "./I-lesson";
+import { ILesson } from './I-lesson';
 
 export const data: ILesson[] = [{
     id: 1,
-    url: 'etyud-zametіl-ferents-lіst',
+    url: 'etyud-zametil-ferents-list',
     title: 'Етюд «Заметіль», Ференц Ліст'
   },
   {
     id: 2,
-    url: 'vals-5-frederіk-shopen',
+    url: 'vals-5-frederik-shopen',
     title: 'Вальс № 5, Фредерік Шопен'
   },
   {
     id: 3,
-    url: 'skertso-1-frederіk-shopen',
+    url: 'skertso-1-frederik-shopen',
     title: 'Скерцо № 1, Фредерік Шопен'
   },
   {
     id: 4,
-    url: 'skertso-4-frederіk-shopen',
+    url: 'skertso-4-frederik-shopen',
     title: 'Скерцо № 4, Фредерік Шопен'
   },
   {
     id: 5,
-    url: 'skertso-2-frederіk-shopen',
+    url: 'skertso-2-frederik-shopen',
     title: 'Скерцо № 2, Фредерік Шопен'
   },
   {
     id: 6,
-    url: 'prelyudіya-13-frederіk-shopen',
+    url: 'prelyudiya-13-frederik-shopen',
     title: 'Прелюдія № 13, Фредерік Шопен'
   },
   {
     id: 7,
-    url: 'skertso-3-frederіk-shopen',
+    url: 'skertso-3-frederik-shopen',
     title: 'Скерцо № 3, Фредерік Шопен'
   },
   {
     id: 8,
-    url: 'mazurka-11-frederіk-shopen',
+    url: 'mazurka-11-frederik-shopen',
     title: 'Мазурка № 11, Фредерік Шопен'
   },
   {
     id: 9,
-    url: 'mazurka-10-frederіk-shopen',
+    url: 'mazurka-10-frederik-shopen',
     title: 'Мазурка № 10, Фредерік Шопен'
   },
   {
     id: 10,
-    url: 'prelyudіya-19-frederіk-shopen',
+    url: 'prelyudiya-19-frederik-shopen',
     title: 'Прелюдія № 19, Фредерік Шопен'
   },
   {
     id: 11,
-    url: 'etyud-10-frederіk-shopen',
+    url: 'etyud-10-frederik-shopen',
     title: 'Етюд № 10, Фредерік Шопен'
   },
   {
     id: 12,
-    url: 'etyud-20-frederіk-shopen',
+    url: 'etyud-20-frederik-shopen',
     title: 'Етюд № 20, Фредерік Шопен'
   },
   {
     id: 13,
-    url: 'muzichnij-tvіr-prihistok-frants-shubert-ferents-lіst',
+    url: 'muzichnij-tvir-prihistok-frants-shubert-ferents-list',
     title: 'Музичний твір «Прихисток», Франц Шуберт, Ференц Ліст'
   },
   {
     id: 14,
-    url: 'noktyurn-7-frederіk-shopen',
+    url: 'noktyurn-7-frederik-shopen',
     title: 'Ноктюрн № 7, Фредерік Шопен'
   },
   {
     id: 15,
-    url: 'balada-4-frederіk-shopen',
+    url: 'balada-4-frederik-shopen',
     title: 'Балада № 4, Фредерік Шопен'
   }
 ];
